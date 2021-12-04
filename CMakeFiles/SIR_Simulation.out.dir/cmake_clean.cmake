09f3a2c7692053f9b1acd86714037461b513d840
Niccolò <niccolo.taddeisozzi@gmail.com>
2021-12-04 16:00:44 +0100

    Setting up CMake

---
+++ CMakeFiles/SIR_Simulation.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIR_Simulation.out.dir/main.cpp.o"
+  "SIR_Simulation.out"
+  "SIR_Simulation.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIR_Simulation.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
